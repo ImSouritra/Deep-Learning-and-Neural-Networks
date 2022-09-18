f0a4100e9e6c15ab19a7f318660c51f52f19f414
ImSouritra <84196931+ImSouritra@users.noreply.github.com>
2022-09-18 12:51:36 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -34,10 +34,15 @@ def visualize(original, augmented):
   plt.imshow(augmented)
   
   
-def preprocess_image(filename):
+def preprocess_and_plot_prediction(filename,model,class_names):
   image = tf.io.read_file(filename)
   image = tf.image.decode_image(image)
   image = tf.image.resize(image,size=(224,224))
   image = image/255.
   image = tf.expand_dims(image,axis=0)
-  return image  
+  predictions = model.predict(image)
+  predicted_label = class_names[np.argmax(predictions)]
+  plt.figure(figsize=(10,7))
+  plt.imshow(image)
+  plt.title(f"Predicted Label: {predicted_label}")
+  plt.axis("off")
