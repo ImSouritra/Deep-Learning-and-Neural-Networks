35bee51f020e56a334ca0a4ee3ff9852ce15082d
ImSouritra <84196931+ImSouritra@users.noreply.github.com>
2022-09-18 12:43:49 +0530

    Create helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,43 @@
+def plot_loss_curves(history):
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+
+    accuracy = history.history["accuracy"]
+    val_accuracy = history.history["val_accuracy"]
+
+    epochs = range(len(history.history["loss"]))
+
+    #plot loss
+    plt.plot(epochs,loss,label="training loss")
+    plt.plot(epochs,val_loss,label="Val loss")
+    plt.title("loss")
+    plt.xlabel("epochs")
+    plt.legend()
+
+    #plot accuracy
+    plt.figure()
+    plt.plot(epochs,accuracy,label="training accuracy")
+    plt.plot(epochs,val_accuracy,label="Val Accuracy")
+    plt.title("accuracy")
+    plt.xlabel("epochs")
+    plt.legend()
+    
+    
+def visualize(original, augmented):
+  fig = plt.figure()
+  plt.subplot(1,2,1)
+  plt.title('Original image')
+  plt.imshow(original)
+
+  plt.subplot(1,2,2)
+  plt.title('Augmented image')
+  plt.imshow(augmented)
+  
+  
+def preprocess_image(filename):
+  image = tf.io.read_file(filename)
+  image = tf.image.decode_image(image)
+  image = tf.image.resize(image,size=(224,224))
+  image = image/255.
+  image = tf.expand_dims(image,axis=0)
+  return image  
