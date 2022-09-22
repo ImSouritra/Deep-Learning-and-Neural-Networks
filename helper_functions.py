b20dc4501005c0165d6b760790c3fa13d0badfeb
ImSouritra <84196931+ImSouritra@users.noreply.github.com>
2022-09-22 11:04:54 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -142,35 +142,43 @@ def make_train_test_split(root_dir,class_names):
     classes = class_names
 
     for cls in classes:
-        os.makedirs(root_dir +'train/' + cls)
-        os.makedirs(root_dir +'test/' + cls)
+    os.makedirs(root_dir +'train/' + cls)
+    os.makedirs(root_dir +'val/' + cls)
+    os.makedirs(root_dir +'test/' + cls)
+    
+## creating partition of the data after shuffeling
 
-    ## creating partition of the data after shuffeling
-
-    for cls in classes:
-        src = root_dir + cls # folder to copy images from
-        print(src)
+for cls in classes:
+    src = root_dir + cls # folder to copy images from
+    print(src)
 
     allFileNames = os.listdir(src)
     np.random.shuffle(allFileNames)
 
-    ## here 0.75 = training ratio , (0.95-0.75) = validation ratio , (1-0.95) =  
+    ## here 0.75 = training ratio , (0.95-0.75) = validation ratio , (1-0.95) = test ratio 
     ##training ratio  
-    train_FileNames,test_FileNames = np.split(np.array(allFileNames),[int(len(allFileNames)*0.95)])
+    train_FileNames,val_FileNames,test_FileNames = np.split(np.array(allFileNames),[int(len(allFileNames)*0.75),int(len(allFileNames)*0.95)])
 
     # #Converting file names from array to list
 
     train_FileNames = [src+'/'+ name for name in train_FileNames]
+    val_FileNames = [src+'/' + name for name in val_FileNames]
     test_FileNames = [src+'/' + name for name in test_FileNames]
 
     print('Total images  : '+ cls + ' ' +str(len(allFileNames)))
     print('Training : '+ cls + ' '+str(len(train_FileNames)))
+    print('Validation : '+ cls + ' ' +str(len(val_FileNames)))
     print('Testing : '+ cls + ' '+str(len(test_FileNames)))
     
     ## Copy pasting images to target directory
 
     for name in train_FileNames:
-        shutil.copy(name, root_dir +'train/'+cls )
+        shutil.copy(name, root_dir + 'train/'+cls )
+
+
+    for name in val_FileNames:
+        shutil.copy(name, root_dir +'val/'+cls )
+
 
     for name in test_FileNames:
-        shutil.copy(name,root_dir +'test/'+cls )
+        shutil.copy(name,root_dir + 'test/'+cls )
